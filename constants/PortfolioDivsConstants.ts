c8c3d094d7d1f7801b180b5d92d4c30b60d32b91
Vitor Pestana <vitor.pestana@prion.com.br>
2025-12-22 16:10:07 -0300

    ultimos detalhes amizade

--- constants/PortfolioDivsConstants.ts
+++ constants/PortfolioDivsConstants.ts
@@ -34,7 +34,9 @@ export const portfolioEsquerda = {
   buracoNegro: {
     bgImage:
       "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2Y5N2VqbjdiaHduZTlqM2g0Y2RkMzVmd213b2VteWkwa3k2ajlodiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Xd7QqFPv4IVAz8dnog/giphy.gif",
-    size: { widthAndHeight: "w-full aspect-video flex-1" },
+    size: {
+      widthAndHeight: "max-h-[110px] sm:max-h-none w-full aspect-video flex-1",
+    },
   },
 };
 
@@ -52,7 +54,10 @@ export const portfolioMeio = {
       routeOrSiteUrl: "",
       style: " bottom-full rounded-t-2xl",
     },
-    size: { widthAndHeight: "w-full aspect-video sm:flex-1" },
+    size: {
+      widthAndHeight:
+        "max-h-[250px] sm:max-h-none w-full aspect-video sm:flex-1",
+    },
   },
   gif: {
     bgImage:
@@ -83,6 +88,6 @@ export const portfolioDireita = {
   niteroi: {
     bgImage:
       "https://images.unsplash.com/photo-1563799744275-a7bbd947b92a?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    size: { widthAndHeight: "w-[30%] aspect-1/2" },
+    size: { widthAndHeight: "max-h-[130px] sm:max-h-none w-[30%] aspect-1/2" },
   },
 };
