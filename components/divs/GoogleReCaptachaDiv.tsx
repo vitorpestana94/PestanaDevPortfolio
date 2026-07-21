4a62f69fd4407de9d49547396ba56b77dc83bf41
Vitor Pestana <vitor.pestana@prion.com.br>
2026-07-21 12:16:04 -0300

    ajeitei o recaptcha para ficar padronizado para interpolação do 18n. tbm ajeitei um detalhe na tradução

--- components/divs/GoogleReCaptachaDiv.tsx
+++ components/divs/GoogleReCaptachaDiv.tsx
@@ -7,30 +7,34 @@ export default function GoogleReCaptachaDiv({
 }: {
    className?: string;
 }) {
-   const t = useTranslations("googleReCaptcha");
+   const t = useTranslations();
 
    return (
       <p
          className={`text-[0.4375rem] text-[#818589] sm:text-[0.5rem] xl:text-[0.5625rem] 3xl:text-[0.625rem] text-center whitespace-pre-line leading-tight px-3 max-w-xs ${className}`}
       >
-         {t("protectedBy")}
-         <a
-            href="https://policies.google.com/privacy"
-            className="hoverPestana"
-            target="_blank"
-            rel="noopener noreferrer"
-         >
-            {t("privacy")}
-         </a>
-         <a
-            href="https://policies.google.com/terms"
-            className="hoverPestana"
-            target="_blank"
-            rel="noopener noreferrer"
-         >
-            {t("terms")}
-         </a>
-         {t("apply")}
+         {t.rich("googleReCaptcha", {
+            terms: (chunks) => (
+               <a
+                  href="https://policies.google.com/privacy"
+                  className="hoverPestana"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  {chunks}
+               </a>
+            ),
+            privacy: (chunks) => (
+               <a
+                  href="https://policies.google.com/terms"
+                  className="hoverPestana"
+                  target="_blank"
+                  rel="noopener noreferrer"
+               >
+                  {chunks}
+               </a>
+            ),
+         })}
       </p>
    );
 }
