5617b10fc1ffd3f011a08cf8aa8b509e40e68ced
Vitor Pestana <vitor.pestana@prion.com.br>
2026-07-30 16:16:26 -0300

    últimos detalhes da primeira parte da session

--- components/images/NasaPictureOfTheDay/NasaPictureOfTheDay.tsx
+++ components/images/NasaPictureOfTheDay/NasaPictureOfTheDay.tsx
@@ -9,7 +9,6 @@ export default function NasaPictureOfTheDay({ url, title, copyRight } : Interfac
     const { ref, isInView } = useEffetInView();
     const effect = { scale: 1.15 };
 
-    
     return (
         <motion.div className="relative w-[75%] md:w-[40%] aspect-4/3"
             ref={ref}
@@ -28,16 +27,19 @@ export default function NasaPictureOfTheDay({ url, title, copyRight } : Interfac
                     fill
                     quality={85}
                     sizes="(max-width: 768px) 90vw, (max-width: 1024px) 60vw, 45vw"
-                    className="object-cover rounded-4xl"
+                    className="object-cover rounded-3xl"
                     priority
                 />
-                <div className="flex flex-col gap-y-0.75 absolute text-gray-300
-                -bottom-9 text-[0.375rem] text-center right-0 left-0
-                md:-bottom-11 md:text-[0.5625rem] md:text-right md:right-3
-                3xl:text-[0.625rem]">
-                    <p>{`Copyright:`}</p>
-                    {`${copyRight}`}
-                </div>
+                {
+                    copyRight &&
+                    <div className="flex flex-col gap-y-0.5 absolute text-gray-400
+                    -bottom-9 text-[0.375rem] text-center right-0 left-0
+                    md:-bottom-10 md:text-[0.5rem] md:text-right md:right-3
+                    3xl:text-[0.625rem]">
+                        <p>{`Copyright:`}</p>
+                        {`${copyRight}`}
+                    </div>
+                }
             </motion.div>
         </motion.div>
     )
