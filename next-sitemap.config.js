9cc81d03c0f825d787195ab9f5099858a78a6887
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2026-09-04 16:15:49 -0300

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.pestanadev.com.br/',
-  generateRobotsTxt: true,
+  siteUrl: 'https://www.pestanadev.com.br/'
 };
