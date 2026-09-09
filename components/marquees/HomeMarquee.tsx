4373b2b5e524e79fad821fff766ae3ba84f002e1
Vitor Pestana <vitor.pestana@prion.com.br>
2026-09-09 12:29:30 -0300

    pequena mudança

--- components/marquees/HomeMarquee.tsx
+++ components/marquees/HomeMarquee.tsx
@@ -1,15 +1,15 @@
 import Marquee from "react-fast-marquee";
-import { skills } from "@/constants/skillsConstants";
+import { skills, skillPerLabel } from "@/constants/skillsConstants";
 
 export default function HomeMarquee() {
   return (
-    <Marquee speed={50} autoFill className="border border-x-0 border-y-[#38b6ff]/45 h-20 bg-[#070a10]">
+    <Marquee speed={50} autoFill className="border border-x-0 border-y-[#38b6ff]/45 h-15 lg:h-20 bg-[#070a10]">
       <div className="flex items-center gap-x-16 z-50">
         {
           skills.flat().map((skill, index) => (
             <span className={`flex items-center gap-x-16 text-[#bfbfbf]
               ${index === 0 ? "ml-16" : ""}`} key={index}>
-              <p >{`${skill.toUpperCase()}`}</p>
+              <p className="text-xs lg:text-[1rem] uppercase">{`${skillPerLabel[skill] ?? skill}`}</p>
               <p className={`text-[1.25rem]`}>•</p>
             </span>
           ))
