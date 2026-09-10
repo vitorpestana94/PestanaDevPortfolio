b027c33f6e3e57c36a7e70686af8d95a2851ff12
Vitor Pestana <vitor.pestana@prion.com.br>
2026-09-10 08:46:24 -0300

    simplifiquei as constantes das skills

--- constants/skillsConstants.ts
+++ constants/skillsConstants.ts
@@ -1,5 +1,10 @@
 import { IconName } from "@/components/icons/Icons";
 
+const backend: IconName[] = ["dotnet", "csharp", "python", "aspnet", "node", "dapper", "entity"];
+const frontend: IconName[] = ["next", "tailwind", "react", "typescript", "js", "html", "css", "bootstrap"]
+const database: IconName[] = ["sql", "sqlServer", "mariadb", "mysql", "postgresql"];
+const tools: IconName[] = ["git", "claude", "visualstudio", "vscode", "gitHub", "bitbucket"];
+
 export const skillPerLabel: { [key: string]: string } = {
    dotnet: ".NET",
    csharp: "C#",
@@ -25,52 +30,40 @@ export const skillPerLabel: { [key: string]: string } = {
    visualstudio: "Visual Studio"
 };
 
-export const skills: IconName[][] = [
-   // Backend
-   ["dotnet", "csharp", "python", "aspnet", "node", "dapper", "entity"],
-
-   // Database
-   ["sql", "sqlServer", "mariadb", "mysql", "postgresql"],
-
-   // Frontend
-   ["next", "tailwind", "react", "typescript", "js", "html", "css", "bootstrap"],
-
-   // Tools
-   ["git", "claude", "visualstudio", "vscode", "gitHub", "bitbucket"],
-];
+export const skills: IconName[] = [...database, ...frontend, ...backend, ...tools];
 
 export const databaseSkills = {
-   [skills[1][0]]: 95,
-   [skills[1][1]]: 93,
-   [skills[1][3]]: 82,
-   [skills[1][2]]: 82,
-   [skills[1][4]]: 75,
+   [database[0]]: 95, // sql
+   [database[1]]: 93, // sqlServer
+   [database[3]]: 82, // mysql
+   [database[2]]: 82, // mariadb
+   [database[4]]: 75, // postgresql
 }
 
 export const backendSkills = {
-   [skills[0][1]]: 95,
-   [skills[0][0]]: 95,
-   [skills[0][3]]: 95,
-   [skills[0][5]]: 93,
-   [skills[0][6]]: 90,
-   [skills[0][2]]: 71,
-   [skills[0][4]]: 53,
+   [backend[1]]: 95, // csharp
+   [backend[0]]: 95, // dotnet
+   [backend[3]]: 95, // aspnet
+   [backend[5]]: 93, // dapper
+   [backend[6]]: 90, // entity
+   [backend[2]]: 71, // python
+   [backend[4]]: 53, // node
 };
 
 export const frontendSkills = {
-   [skills[2][0]]: 95,
-   [skills[2][3]]: 93,
-   [skills[2][4]]: 93,
-   [skills[2][1]]: 90,
-   [skills[2][2]]: 90,
-   [skills[2][7]]: 72,
+   [frontend[0]]: 95, // next
+   [frontend[3]]: 93, // typescript
+   [frontend[4]]: 93, // js
+   [frontend[1]]: 90, // tailwind
+   [frontend[2]]: 90, // react
+   [frontend[7]]: 72, // bootstrap
 };
 
 export const toolsSkills = {
-   [skills[3][0]]: 95,
-   [skills[3][2]]: 93,
-   [skills[3][4]]: 92,
-   [skills[3][5]]: 92,
-   [skills[3][1]]: 90,
-   [skills[3][3]]: 81,
+   [tools[0]]: 95, // git
+   [tools[2]]: 93, // visualstudio
+   [tools[4]]: 92, // gitHub
+   [tools[5]]: 92, // bitbucket
+   [tools[1]]: 90, // claude
+   [tools[3]]: 81, // vscode
 };
