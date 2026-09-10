e91b294e12ece546bf2ac6fdca918449a86cbf40
Vitor Pestana <vitor.pestana@prion.com.br>
2026-09-10 11:48:19 -0300

    quase terminado

--- components/sections/home/HomeSecondSection.Values.tsx
+++ components/sections/home/HomeSecondSection.Values.tsx
@@ -1,11 +1,15 @@
 import Wrapper from "@/components/wrappers/home/HomeWrapper";
 import SobreSeuZeLeiteiro from "@/components/divs/home/SecondSession/HomeTextAndPicture";
+import MyValues from "@/components/titles/MyValuesTitle";
 
 export default function HomeSecondSectionValues() {
    return (
     <section id="valores" className="homeSectionMargin relative">
-        <Wrapper className="conteudoWrapper homeMobileDirection sm:flex-row">
-            <SobreSeuZeLeiteiro />
+        <Wrapper className="conteudoWrapper space-y-15 sm:space-y-20 xl:space-y-25">
+            <MyValues />
+            <div className="homeMobileDirection sm:flex-row">
+                <SobreSeuZeLeiteiro />
+            </div>
         </Wrapper>
     </section>
    );
