4de7c97d569a25e80da7641a646a7dd2237e2248
Vitor Pestana <vitor.pestana@prion.com.br>
2026-09-11 09:13:01 -0300

    melhorei o front da landing page

--- components/sections/home/HomeSecondSection.tsx
+++ components/sections/home/HomeSecondSection.tsx
@@ -1,11 +1,10 @@
 import Wrapper from "@/components/wrappers/home/HomeWrapper";
 import SobreMim from "@/components/divs/home/SecondSession/HomeAboutTextDiv";
 import ProfilePictureDesktop from "@components/divs/ProfilePictureDiv.DesktopTablet";
-import UmPoucoSobreMim from "@/components/titles/AbouTitle";
 
 export default function HomeSecondSection() {
    return (
-      <section id="sobre" className="homeSectionMargin relative">
+      <section id="sobre" className="relative diferentSection">
          <Wrapper className="conteudoWrapper homeMobileDirection sm:flex-row">
             <SobreMim />
             <ProfilePictureDesktop />
