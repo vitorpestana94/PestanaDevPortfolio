c4445ef1b8eecb76444dfae540ea244f13403634
Vitor Pestana <vitor.pestana@prion.com.br>
2026-09-11 16:08:09 -0300

    quase pronto

--- components/spans/experienceSpan/useExperienceSpan.ts
+++ components/spans/experienceSpan/useExperienceSpan.ts
@@ -1,10 +1,15 @@
 import options from "data/options.json";
+import { useTranslations } from "next-intl";
 
 export default function useExperienceSpan(){
-    const initialExpDate = new Date(options.initialExpDate);
+    const initialExpDate = new Date(options.experiece.initialExpDate);
     const today = new Date();
+    const t = useTranslations("home.segundaSessao.experience")
  
     return { 
-        years: today.getFullYear() - initialExpDate.getFullYear() 
+        years: (today.getFullYear() - initialExpDate.getFullYear()).toString(),
+        projects: options.experiece.projects,
+        academic: options.experiece.academic,
+        t
     };
 }
--- constants/metaData.ts
+++ constants/metaData.ts
@@ -1,3 +1,3 @@
 export const title: string = "Pestana Dev";
-export const description: string = "Olá! Sou um desenvolvedor Web Full Stack! Hello, i'm a full stack web developer!"
+export const description: string = "Hello, i'm a full stack web developer!\nOlá! Sou um desenvolvedor Web Full Stack!"
 export const imageForMetaData: string = "/icons/vP.svg"
