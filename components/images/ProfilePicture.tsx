787a56457eed67136ea8fbe1e1914e1d21ac163a
Vitor Pestana <vitor.pestana@prion.com.br>
2026-09-16 08:29:18 -0300

    profile picture atualizada

--- components/images/ProfilePicture.tsx
+++ components/images/ProfilePicture.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function ProfilePicture() {
 
    return (
-      <div className="aspect-square z-0 w-[30%] xl:w-[35%] relative z-0">
+      <div className="aspect-square z-0 w-[30%] xl:w-[35%] relative">
          <Image
             className="rounded-full z-0"
             src={"/png/profilePicture.png"}
             alt="pestanaDevPicture"
             fill
             sizes="(min-width: 1280px) 35vw, 30vw"
-            style={{ objectFit: "fill" }}
+            style={{ objectFit: "cover" }}
             loading="lazy"
          />
       </div>
