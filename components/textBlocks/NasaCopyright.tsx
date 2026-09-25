92151b5cc88783a26d579f855b3ff2b3a9522cd5
Vitor Pestana <vitor.pestana@prion.com.br>
2026-09-25 09:42:24 -0300

    pequenos ajustes

--- components/textBlocks/NasaCopyright.tsx
+++ components/textBlocks/NasaCopyright.tsx
@@ -5,7 +5,7 @@ export default function NasaCopyright({ copyright, className } : Interface){
         <>
         {
             copyright &&
-            <div className={`flex flex-col gap-y-px text-gray-400 max-w-60
+            <div className={`flex flex-col gap-y-px text-gray-400 
             text-[0.375rem] text-center mt-1.5
             md:text-[0.5rem] md:text-right md:mt-0
             3xl:text-[0.625rem]
